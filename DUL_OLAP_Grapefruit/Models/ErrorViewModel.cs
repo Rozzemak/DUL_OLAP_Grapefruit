50b71e72b2ffb034201e6836801fcb6ecbcde962
Marek Slavka <marek.slavka97@gmail.com>
2019-12-04 02:57:18 +0100

    Add project files.

---
+++ DUL_OLAP_Grapefruit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DUL_OLAP_Grapefruit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
