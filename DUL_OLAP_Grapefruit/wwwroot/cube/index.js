1bf9ae762042aeb1bd9327e1cd50cfd92c02dffb
Marek Slavka <Rozzemak@gmail.com>
2019-12-18 04:11:39 +0100

    Front end edits

---
+++ DUL_OLAP_Grapefruit/wwwroot/cube/index.js
@@ -0,0 +1,23 @@
+﻿
+//
+// initialize pivot engine
+const  panel = new wijmo.olap.PivotPanel("#pivotPanel");
+const grid = new wijmo.olap.PivotGrid("#pivotGrid");
+const engine = new wijmo.olap.PivotEngine();
+engine.itemsSource = "http://localhost:11111/api/dataengine/cube";
+engine.rowFields.push(new wijmo.olap.CubePivotField(engine, "[Customer].[Country]", "Country"));
+engine.columnFields.push(new wijmo.olap.CubePivotField(engine, "[Customer].[Occupation]", "Occupation"));
+engine.valueFields.push(new wijmo.olap.CubePivotField(engine, "[Measures].[Customer Count]", "Customer Count"));
+panel.engine = engine;
+grid.itemsSource = panel;
+
+/*
+var ng = new wijmo.olap.PivotEngine({
+    itemsSource: "http://localhost:11111/api/dataengine/cube"
+});
+
+var pivotChart = new wijmo.olap.PivotChart('#pivotChart', {
+    itemsSource: ng,
+    showTitle: true,
+    showLegend: 'Auto'
+});*/
